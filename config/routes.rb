a19fd62784df200c0bd96c6bb0b61b88abb2030f
Kurihara-D <d.kurihara0318@gmail.com>
2022-11-12 06:43:24 +0000

    add users show from codespaces!

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
 
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :create, :show]
 end
